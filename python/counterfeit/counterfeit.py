d1474a105a793690d7361b9883fb673e395390ab
bannedit <bannedit0@gmail.com>
2014-04-07 05:45:39 -0400

    Initial working version

--- python/counterfeit/counterfeit.py
+++ python/counterfeit/counterfeit.py
@@ -25,10 +25,12 @@ PAGE_READWRITE         = 0x04
 PAGE_EXECUTE_READWRITE = 0x40
 PAGE_NOACCESS          = 0x01
 
+kernel32 = windll.kernel32
+
 class counterfeit():
 	def __init__(self):
 		self.pid = pykd.getCurrentProcessId()
-		self.hprocess = windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, self.pid)
+		self.hprocess = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, self.pid)
 
 	def banner(self):
 		'''
@@ -58,7 +60,12 @@ class counterfeit():
 		Change the page permissions for a given memory address.
 		'''
 		self.size = dwSize
-		flOldProtect = kernel32.VirtualProtectEx(lpAddress, dwSize, flNewProtect, flOldProtect)
+		flOldProtect = byref(create_string_buffer("\x00" * 4))
+		ret = kernel32.VirtualProtectEx(self.hprocess, lpAddress, dwSize, flNewProtect, flOldProtect)
+
+		if not ret:
+			print "VirtualProtectEx failed"
+
 		return flOldProtect
 
 	def fill_pattern(self, address):
@@ -81,30 +88,32 @@ def main():
 	desc = "counterfeit v1.0 - handy script to prototype exploitation of memory corruption and use-after-free vulnerabilities"
 	parser = argparse.ArgumentParser(description = desc,
 				usage='%(prog)s [options]')
-	parser.add_argument("-a", "--allocate", metavar = "size", action = "store_true", help = "Allocate memory RWX")
-	parser.add_argument("-p", "--protect", metavar = ("address", "size", "protection"), nargs = 3, action = "store_true", 
+	parser.add_argument("-a", dest = "alloc", metavar = "size", help = "Allocate memory RWX")
+	parser.add_argument("-p", dest = "protect", metavar = ("addr", "size", "protect"), nargs = 3, 
 				help = "Change page permissions ['r', 'rwx', 'none']")
 	parser.add_argument("-f", "--fill", action = "store_true",  help = "Fill the memory with a pattern")
 
 	args = parser.parse_args()
 	cf = counterfeit()
 	cf.banner()
+	print args
 
-	if args.allocate:
-		addr = cf.VirtualAllocEx(0, int(size, 16), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)
+	if args.alloc:
+		addr = cf.VirtualAllocEx(0, int(args.allocate, 16), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)
 		if args.fill:
 			cf.fill_pattern(addr)
 		print "Allocated memory @ 0x%08x with RWX permissions.\n" % addr
 		return
 
 	if args.protect:
+		address, size, protection = args.protect
 		if (protection == 'r'):
 			flProtect = PAGE_READ
-		elif (protection = 'rw'):
+		elif (protection == 'rw'):
 			flProtect = PAGE_READWRITE
-		elif (protection = 'rwx'):
+		elif (protection == 'rwx'):
 			flProtect = PAGE_EXECUTE_READWRITE
-		elif (protection = 'none'):
+		elif (protection == 'none'):
 			flProtect = PAGE_NOACCESS
 		else:
 			# Default to PAGE_NOACCESS
@@ -113,8 +122,11 @@ def main():
 		cf.VirtualProtectEx(int(address, 16), int(size, 16), flProtect, 0)
 		if args.fill:
 			cf.fill_pattern(int(address, 16))
-		print "Changed permissions for memory @ 0x%08x to '%s'\n" % (int(address, 16), protection))
+		print "Changed permissions for memory @ 0x%08x to '%s'\n" % (int(address, 16), protection)
 		return
+
+	if not args.alloc and not args.protect:
+		parser.print_help()
 
 if __name__ == "__main__":
 	main()
